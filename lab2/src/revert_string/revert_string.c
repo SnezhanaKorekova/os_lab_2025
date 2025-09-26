d6bb77713414977d64d27f55f1cff1c6e39dcaa4
SnezhanaKorekova <94750455+SnezhanaKorekova@users.noreply.github.com>
2025-09-26 14:34:36 +0000

    Лаб2 задание2: реализована функция RevertString

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,21 @@
 #include "revert_string.h"
+#include <string.h>
 
 void RevertString(char *str)
 {
-	// your code here
+    int length = strlen(str);
+    int i = 0;
+    int j = length - 1;
+    
+    // Меняем симметричные символы местами до середины строки
+    while (i < j)
+    {
+        // Обмен значений через временную переменную
+        char temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+        
+        i++;
+        j--;
+    }
 }
-
